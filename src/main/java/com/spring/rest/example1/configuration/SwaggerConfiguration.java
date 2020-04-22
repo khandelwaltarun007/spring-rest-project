ccd6d64d0e5793566a3dc8ad80afcb8b25c15f77
tarkhand <Tarun.Khandelwal1@amdocs.com>
2020-04-23 00:09:25 +0530

    added Swagger dependency and configuration

--- src/main/java/com/spring/rest/example1/configuration/SwaggerConfiguration.java
+++ src/main/java/com/spring/rest/example1/configuration/SwaggerConfiguration.java
@@ -1,18 +1,33 @@
-/*
- * package com.spring.rest.example1.configuration;
- * 
- * import org.springframework.context.annotation.Configuration;
- * 
- * import springfox.documentation.builders.PathSelectors; import
- * springfox.documentation.builders.RequestHandlerSelectors; import
- * springfox.documentation.spi.DocumentationType; import
- * springfox.documentation.spring.web.plugins.Docket; import
- * springfox.documentation.swagger2.annotations.EnableSwagger2;
- * 
- * @Configuration
- * 
- * @EnableSwagger2 public class SwaggerConfiguration { ` public Docket api() {
- * return new
- * Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any
- * ()) .paths(PathSelectors.any()).build(); } }
- */
+
+package com.spring.rest.example1.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+	/* This method generating error with hateoas
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}*/
+	
+	@Bean
+	public LinkDiscoverers api() {
+	List<LinkDiscoverer> plugins = new ArrayList<>();
+    plugins.add(new CollectionJsonLinkDiscoverer());
+    return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+	}
+}
