2f6a6ca07027245ccf38f8f0555e92059613fb9e
tarkhand <Tarun.Khandelwal1@amdocs.com>
2020-04-24 01:51:31 +0530

    removing uri level authorization

--- src/main/java/com/spring/rest/example1/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/spring/rest/example1/configuration/SpringSecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 		.antMatchers("/").permitAll()
-		.antMatchers("/employee/**").access("hasAnyAuthority('ADMIN')")
+		//.antMatchers("/employee/**").access("hasAnyAuthority('ADMIN')")
 		.and()
 		.httpBasic().realmName("MY_APP_REALM").authenticationEntryPoint(appAuthenicationEntryPoint).and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
