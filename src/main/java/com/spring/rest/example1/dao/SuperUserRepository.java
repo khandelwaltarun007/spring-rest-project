1facc702d7cf85e58f06b03bcac2c11eda4fee59
tarkhand <Tarun.Khandelwal1@amdocs.com>
2020-04-24 01:57:49 +0530

    enhancing super user creation at the time of application load

--- src/main/java/com/spring/rest/example1/dao/SuperUserRepository.java
+++ src/main/java/com/spring/rest/example1/dao/SuperUserRepository.java
@@ -1,14 +1,15 @@
 package com.spring.rest.example1.dao;
 
+import java.util.Date;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.spring.rest.example1.model.Employee;
 import com.spring.rest.example1.model.User;
 
 @Repository
@@ -23,10 +24,20 @@ public class SuperUserRepository {
 		Optional<User> optionalUser = userRepository.findById(Long.valueOf(1));
 		User existingUser = optionalUser.orElse(null);
 		if (existingUser == null) {
+			Employee employee = new Employee();
+			employee.setName("Admin");
+			employee.setDesignation("Super User");
 			User user = new User();
-			user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+			user.setPassword("admin");
 			user.setUsername("admin");
 			user.setRole("ADMIN");
+			user.setStatus("1");
+			user.setCreatedBy("System");
+			user.setUpdatedBy("System");
+			user.setCreatedDate(new Date());
+			user.setUpdatedDate(new Date());
+			user.setEmployee(employee);
+			employee.setUser(user);
 			userRepository.save(user);
 		}
 	}
