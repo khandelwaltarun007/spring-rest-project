a942579e27cf847dcc2e72ed200ce992539f933d
tarkhand <Tarun.Khandelwal1@amdocs.com>
2020-04-26 11:11:06 +0530

    Added string not empty utility

--- src/main/java/com/spring/rest/example1/util/CodeUtility.java
+++ src/main/java/com/spring/rest/example1/util/CodeUtility.java
@@ -5,7 +5,11 @@ public class CodeUtility {
 	public static boolean isNotEmpty(Object obj) {
 		return obj != null;
 	}
-	
+
+	public static boolean isNotEmpty(String str) {
+		return str != null && str.length() > 0;
+	}
+
 	public static boolean isEmpty(Object obj) {
 		return obj == null;
 	}
