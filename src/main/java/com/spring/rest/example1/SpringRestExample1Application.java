a1d98c7b7b5250facf1809bbeb9f48e7be4a0dab
tarkhand <Tarun.Khandelwal1@amdocs.com>
2020-04-26 11:12:32 +0530

    added Hypermedia support feature in spring

--- src/main/java/com/spring/rest/example1/SpringRestExample1Application.java
+++ src/main/java/com/spring/rest/example1/SpringRestExample1Application.java
@@ -5,10 +5,13 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = {HypermediaType.HAL})
 public class SpringRestExample1Application {
 
 	public static void main(String[] args) {
